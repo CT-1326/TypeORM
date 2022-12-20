cc2fbf0b68b9050ec223b2b017eb7c6385fb37cd
DESKTOP-4BKRPIN\withnetworks <smw1326@withnetworks.com>
2022-12-20 10:55:15 +0900

    create Router

--- test-git/src/index.ts
+++ test-git/src/index.ts
@@ -1,20 +1,16 @@
 import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import * as express from "express";
+import userRouter from "./routers/userRouter";
 
-AppDataSource.initialize().then(async () => {
+const app = express();
+app.use(express.json());
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+AppDataSource
+    .initialize()
+    .then(async () => { })
+    .catch(error => console.error(error));
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
-
-}).catch(error => console.log(error))
+app.use("/api", userRouter);
+app.listen(3000, () => {
+    console.log("Server running");
+});
---
+++ test-git/src/routers/userRouter.ts
@@ -0,0 +1,14 @@
+import * as express from "express";
+import UserController from "../controllers/UserController";
+
+const userRouter = express.Router();
+
+let userController = new UserController();
+
+userRouter.post("/addUser", userController.addUser);
+userRouter.get("/user/:firstName", userController.getUser);
+userRouter.get("/allUsers", userController.getAlllUsers);
+userRouter.put("/user/:firstName", userController.updateUser);
+userRouter.delete("/user/:firstName", userController.deleteUser);
+
+export default userRouter;

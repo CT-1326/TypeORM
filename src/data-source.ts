5a61c2dc7daef6d3944de782de4155f02fa1d7c8
DESKTOP-4BKRPIN\withnetworks <smw1326@withnetworks.com>
2022-12-20 13:44:10 +0900

    using dotenv

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,14 +2,15 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 import { Chat } from "./entity/Chat"
+require('dotenv').config();
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    host: process.env.HOST,
+    port: Number(process.env.PORT),
+    username: process.env.USER_NAME,
+    password: process.env.USER_PASSWORD,
+    database: process.env.DB,
     synchronize: true,
     logging: false,
     entities: [User, Chat],
